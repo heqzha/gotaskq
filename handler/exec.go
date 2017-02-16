0825617380fb8cc11e5642f788b4581bfc04ebfe
Hequn Zhang <heqzha@gmail.com>
2017-02-16 18:43:22 +0800

    Unfinished

--- handler/exec.go
+++ handler/exec.go
@@ -1,13 +1,16 @@
 package handler
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 func ExeSync(outputer func(io.Reader, interface{}) error, outputerArgs interface{}, name string, arg ...string) error {
-	fmt.Printf("Exec: %s %v\n", name, arg)
+	fmt.Printf("Exec: %s %v\n", name, strings.Join(arg, " "))
 	cmd := exec.Command(name, arg...)
 	out, err := cmd.StdoutPipe()
 	if err != nil {
@@ -16,9 +19,18 @@ func ExeSync(outputer func(io.Reader, interface{}) error, outputerArgs interface
 	if err := cmd.Start(); err != nil {
 		return err
 	}
-	if err := outputer(out, outputerArgs); err != nil {
-		return err
-	}
+	buf := new(bytes.Buffer)
+	go func() {
+		n, err := buf.ReadFrom(out)
+		if err != nil {
+			panic(err.Error())
+		}
+		buf.WriteTo(os.Stdout)
+		fmt.Printf("number of bytes: %d\n", n)
+	}()
+	// if err := outputer(out, outputerArgs); err != nil {
+	// 	return err
+	// }
 	if err := cmd.Wait(); err != nil {
 		return err
 	}
